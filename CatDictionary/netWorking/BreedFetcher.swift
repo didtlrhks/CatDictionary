575913ecd5b69a5e43496feb1e0333e6a126e264
sikwan <yysskk99999@naver.com>
2024-01-25 17:55:28 +0900

    View

--- CatDictionary/netWorking/BreedFetcher.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BreedFetcher.swift
-//  CatDictionary
-//
-//  Created by 양시관 on 1/25/24.
-//
-
-import Foundation
--- CatDictionary/netWorking/ErrorView.swift
+++ CatDictionary/netWorking/ErrorView.swift
@@ -5,4 +5,32 @@
 //  Created by 양시관 on 1/25/24.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct ErrorView: View {
+    @ObservedObject var breedFetcher: BreedFetcher
+    var body: some View {
+        VStack {
+            
+            Text("😿")
+                .font(.system(size: 80))
+            
+            Text(breedFetcher.errorMessage ?? "")
+            
+            Button {
+                breedFetcher.fetchAllBreeds()
+            } label: {
+                Text("Try again")
+            }
+
+            
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView(breedFetcher: BreedFetcher())
+    }
+}
