0a5fb7596a36121b835689bce43b3810a5b3f56d
sikwan <yysskk99999@naver.com>
2024-01-25 17:50:44 +0900

    init

---
+++ CatDictionary/CatDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatDictionaryApp.swift
+//  CatDictionary
+//
+//  Created by 양시관 on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CatDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
