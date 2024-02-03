c4fa3b737fa324daba5620502340bd357eb8a789
sikwan <yysskk99999@naver.com>
2024-02-03 18:27:09 +0900

    BreedRow

--- CatDictionary/View+Pro/BreedRow.swift
+++ CatDictionary/View+Pro/BreedRow.swift
@@ -14,26 +14,26 @@ struct BreedRow: View {
     var body: some View {
         HStack {
             
-            if breed.image?.url != nil {
-                AsyncImage(url: URL(string: breed.image!.url!)) { phase in
-                    if let image = phase.image {
+            if breed.image?.url != nil { // 브리드안에 이미지안에 url 이 있다면 닐을반환하지않고
+                AsyncImage(url: URL(string: breed.image!.url!)) { phase in // 여기에서 로딩상태에 따른 뷰를 반환하기위한 용도로 어싱크 이미지를 사용하였고
+                    if let image = phase.image {// 만약에 이미지가 들어오게 되면 이미지의 크기나 등등이 처리된 뷰를 반환하고
                         image.resizable()
                             .scaledToFill()
                             .frame(width: imageSize, height: imageSize)
                             .clipped()
                         
-                     } else if phase.error != nil {
+                     } else if phase.error != nil { // 그게아닌경우 error 를 보여주는 뷰를 반환하고
                          
                          Text(phase.error?.localizedDescription ?? "error")
                              .foregroundColor(Color.pink)
                              .frame(width: imageSize, height: imageSize)
                      } else {
-                        ProgressView()
+                        ProgressView() // 그게아니라면 RrogressView 를 반환하면서 로딩을 하고 있다는걸 보여준다
                              .frame(width: imageSize, height: imageSize)
                      }
                     
                 }
-            }else {
+            } else {
                 Color.gray.frame(width: imageSize, height: imageSize)
             }
             
